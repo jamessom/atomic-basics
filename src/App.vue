73def996a895048500f0f27cab6f7039dded0e3d
Jamessom Queiroz <jamessomqueiroz@gmail.com>
2022-08-14 02:43:44 -0300

    add: nav bar molecule

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="app">
     <Logo />
+    <NavBar />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { Logo } from '@/components/atoms'
+import { NavBar } from '@/components/molecules'
 
 export default defineComponent({
   name: 'App',
-  components: { Logo },
+  components: { Logo, NavBar },
 })
 </script>
 
