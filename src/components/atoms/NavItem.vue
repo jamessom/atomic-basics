dc9983390a0fcd124517e9a239639f5cd54911b1
Jamessom Queiroz <jamessomqueiroz@gmail.com>
2022-08-14 02:39:40 -0300

    feat: add atom nav item

---
+++ src/components/atoms/NavItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <a class="nav-item" :src="to">
+    {{ text }}
+  </a>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'NavItem',
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+    to: {
+      type: String,
+      required: true,
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.nav-item {
+  color: #293947;
+  text-decoration: none;
+  font-size: 1.3rem;
+}
+</style>
--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,1 +1,2 @@
 export { default as Logo } from './TheLogo.vue'
+export { default as NavItem } from './NavItem.vue'
